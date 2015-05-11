f45609f25b4198a2bce4196fb83b811156ad866d
Chhun <Chhun.so@gmail.com>
2015-05-11 11:06:13 -0700

    inital styling

--- db/schema.rb
+++ db/schema.rb
@@ -23,14 +23,11 @@ ActiveRecord::Schema.define(version: 20150510164043) do
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
     t.integer  "log_id"
-    t.integer  "user_id"
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
   end
-
-  add_index "entries", ["user_id"], name: "index_entries_on_user_id", using: :btree
 
   create_table "logs", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -56,5 +53,4 @@ ActiveRecord::Schema.define(version: 20150510164043) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-  add_foreign_key "entries", "users"
 end
