4480befc77fae3cea5a8123a16c22d492e56e17d
Rachel Zuzu <rachelzuzu4@gmail.com>
2015-05-11 14:12:45 -0700

    removed line about merge conflict

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_one :log
-<<<<<<< HEAD
-=======
-  has_many :entries
-  # has_one :entry
->>>>>>> c593921ae34f21e837f389c369bd5aa8f4f4a097
+
   
   # more devise stuff
   def in_role?(role)
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,40 @@ Rails.application.routes.draw do
 
 root 'site#index'
 
-  get "/login", to: "sessions#new"
+devise_for :users
 
-  post "/sessions", to: "sessions#create"
+  # get '/about', to: 'site#about', as: 'about'
 
-  get "/sign_up", to: "users#new", as: "sign_up"
+  # get '/contact', to: 'site#contact', as: 'contact'
 
-  devise_for :users
+  # get '/users/:user_id', to: 'users#index'
 
+  # get '/users/:user_id/log', to: 'logs#show', as: 'user_log'
+
+  # get '/users/:user_id/log/entries', to: 'entries#index', as: 'entries'
+
+  # post '/users/:user_id/log/entries', to: 'entries#create'
+
+  # get '/users/:user_id/log/entries/new', to: 'entries#new', as: 'new_entry'
+
+  # get '/users/:user_id/log/entries/:id', to: 'entries#show', as: 'entry'
+
+  # get '/users/:user_id/log/entries/:id/edit', to: 'entries#edit', as: 'edit_entry'
+
+  # put '/users/:user_id/log/entries/:id/update', to: 'entries#update'
+  
+  # delete '/users/:user_id/log/entries/:id/destroy', to: 'entries#destroy'
+
+
+
+
+
+
+get "/login", to: "sessions#new"
+
+post "/sessions", to: "sessions#create"
+
+get "/sign_up", to: "users#new", as: "sign_up"
 
 resources :users do
 	resources :logs do
