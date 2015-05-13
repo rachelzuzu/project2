f6a9d45fa839b7eb156393b202796033cd567007
Kristina Chang <kristinachang@Kristina-Changs-MacBook-Pro.local>
2015-05-13 15:28:18 -0700

    added search controller for api

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -5,112 +5,85 @@ class EntriesController < ApplicationController
 
   before_action :authenticate_user!
  
- # API CALL
   def index
-    search = params[:q]
-    if search
-      resp = Typhoeus.get(
-        "http://api.nal.usda.gov/usda/ndb/search",
-        params: {
-          format: "json",
-          q: search,
-          sort: "n",
-          max: 30,
-          offset: 0,
-          api_key: "bT0Q1R0Js9aaOsjTR9ro6Oax1y21Wg2J8fmr74Vc"
-        }
-      )
-      @foods = JSON.parse(resp.body)["list"]
-    else 
-      @foods = []
+    # to reference, entries are tied to log id, so reference log_id as in rake routes
+    # so you can find log in your entries index view
+    @log=Log.find(params[:log_id])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @log.entries }
     end
   end
-
-  def food
-    report = params[:q]
-    if report 
-      resp = Typhoeus.get(
-        "http://api.nal.usda.gov/usda/ndb/reports",
-        params: {
-          format: "fjson",
-          ndbno: report,
-          type: "b",
-          api_key: "bT0Q1R0Js9aaOsjTR9ro6Oax1y21Wg2J8fmr74Vc"
-        }
-      )
-      @food = JSON.parse(resp.body)
-    else
-      @food = {}
-    end
-  end
-
-
-
-
-#   def index
-#     # to reference, entries are tied to log id, so reference log_id as in rake routes
-#     # so you can find log in your entries index view
-#     @log=Log.find(params[:log_id])
-
-#     respond_to do |format|
-#       format.html
-#       format.json { render json: @log.entries }
-#     end
-#   end
 	
-# 	def show
-#     @user = current_user
-#     # find the article we are interested in
-#     # pass in params[:id] to get :id parameter from request
-#     # @ is an instance variable to hold a reference to the entry object
-#     @entry = Entry.find(params[:id])
-# 	end
+	def show
+    @user = current_user
+    # find the article we are interested in
+    # pass in params[:id] to get :id parameter from request
+    # @ is an instance variable to hold a reference to the entry object
+    @entry = Entry.find(params[:id])
+	end
 
-# 	def new
-#     @entry=Entry.new
-#     @log=Log.find(params[:log_id])
-# 	end
+	def new
+    @entry=Entry.new
+    @log=Log.find(params[:log_id])
+	end
 
-# 	def edit
-#     @entry=Entry.find(params[:id])
-#     @log=Log.find(params[:log_id])
-# 	end
+	def edit
+    @entry=Entry.find(params[:id])
+    @log=Log.find(params[:log_id])
+	end
 
-#   def create
-#     @log=Log.find(params[:log_id])
-#     @entry = @log.entries.new(entry_params)
+  def create
+    @log=Log.find(params[:log_id])
+    # @food_details = Entry.get_food_details(params[:ndbno_id])
+    @entry = @log.entries.new(entry_params)
 
-# # saving the model in the database
-#     if @entry.save
-#       redirect_to user_log_entry_path(current_user, @entry.log, @entry)
-#     else
-#       render 'new'
-#     end
-#   end
+  # def self.get_details(ndbno_id)
+  #   resp = Typhoeus.get(
+  #       "http://api.nal.usda.gov/usda/ndb/reports",
+  #       params: {
+  #         format: "json",
+  #         ndbno: nbdno_id,
+  #         type: "b",
+  #         api_key: "bT0Q1R0Js9aaOsjTR9ro6Oax1y21Wg2J8fmr74Vc"
+  #       }
+  #     )
+  #   @food = JSON.parse(resp.body)["report"]
+  #   return @food
+  # end
 
-#   def update
-#     @entry = Entry.find(params[:id])
+# saving the model in the database
+    if @entry.save
+      redirect_to user_log_entry_path(current_user, @entry.log, @entry)
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @entry = Entry.find(params[:id])
     
-#     if @entry.update(entry_params)
-#       # redirect back to the show page with the way our routes are nested
-#       # @entry.log can also be params[:log_id]
-#       redirect_to user_log_entry_path(current_user, @entry.log, @entry)
-#     else
-#       render 'edit'
-#     end
-#   end
+    if @entry.update(entry_params)
+      # redirect back to the show page with the way our routes are nested
+      # @entry.log can also be params[:log_id]
+      redirect_to user_log_entry_path(current_user, @entry.log, @entry)
+    else
+      render 'edit'
+    end
+  end
 
-#   def destroy
-#     @entry = Entry.find(params[:id])
-#     @entry.destroy
+  def destroy
+    @entry = Entry.find(params[:id])
+    @entry.destroy
 
-#       redirect_to user_log_entries_path(current_user, @entry.log)
-#   end
+      redirect_to user_log_entries_path(current_user, @entry.log)
+  end
 
-# # made private to make sure it can't be called outside its intended context
-# 	private
-# 		def entry_params
-# 			params.require(:entry).permit(:date, :food, :mood, :avatar)
-# 		end
+# made private to make sure it can't be called outside its intended context
+	private
+		def entry_params
+			params.require(:entry).permit(:date, :food, :mood, :avatar)
+		end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
 root 'site#index'
 
+get '/search', to: 'search#index'
+get '/report', to: 'search#report'
+
+
 # devise_for :users
 devise_for :users do
   get "/users/sign_out" => "devise/sessions#destroy", :as => :destroy_user_session
