97f4e650ed11835e805a8adb6c7ee590763d3074
Kristina Chang <kristinachang@Kristina-Changs-MacBook-Pro.local>
2015-05-13 22:00:18 -0700

    added report model and usda api

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,41 @@
+class ReportsController < ApplicationController
+
+	def index
+    	search = params[:q]
+    	if search
+     	resp = Typhoeus.get(
+        	"http://api.nal.usda.gov/usda/ndb/search",
+       		params: {
+          	format: "json",
+          	q: search,
+          	sort: "n",
+          	max: 30,
+          	offset: 0,
+          	api_key: "bT0Q1R0Js9aaOsjTR9ro6Oax1y21Wg2J8fmr74Vc"
+        	}
+      	)
+      	@foods = JSON.parse(resp.body)["list"]["item"]
+    	else 
+      		@foods = []
+    	end
+  	end
+
+  	def show
+    	report = params[:q]
+    	if report 
+      	resp = Typhoeus.get(
+        	"http://api.nal.usda.gov/usda/ndb/reports",
+        	params: {
+          	format: "json",
+          	ndbno: report,
+          	type: "b",
+          	api_key: "bT0Q1R0Js9aaOsjTR9ro6Oax1y21Wg2J8fmr74Vc"
+        	}
+      	)
+      	@food = JSON.parse(resp.body)["report"]
+    	else
+      		@food = {}
+    	end
+  	end
+
+end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,6 +1,6 @@
 class Entry < ActiveRecord::Base
 	belongs_to :log
-
+	has_many :reports
 
 	# paperclip
     has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
