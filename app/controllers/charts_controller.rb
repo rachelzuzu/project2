517419b61eede3b0635459cedcd45b951408305f
Cristina Reames <cristina.reames@gmail.com>
2015-05-14 18:39:40 -0700

    re-add google charts

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,19 +1,19 @@
-class ChartsController < ApplicationController
+# class ChartsController < ApplicationController
 
-  def index
-    #  @log=Log.find(params[:id])
+#   def index
+#     #  @log=Log.find(params[:id])
 
-    # respond_to do |format|
-    #   format.html
-    #   format.json { render json: @log.entries }
-    end
-  end
+#     # respond_to do |format|
+#     #   format.html
+#     #   format.json { render json: @log.entries }
+#     end
+#   end
 
-  def show
-    #  @user = current_user
-    #   # pass in params[:id] to get :id parameter from request
-    # # @ is an instance variable to hold a reference to the entry object
-    # @chart = Chart.find(params[:id])
-  end
+#   def show
+#     #  @user = current_user
+#     #   # pass in params[:id] to get :id parameter from request
+#     # # @ is an instance variable to hold a reference to the entry object
+#     # @chart = Chart.find(params[:id])
+#   end
 
-end
+# end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150514032623) do
+ActiveRecord::Schema.define(version: 20150514011425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,15 +49,9 @@ ActiveRecord::Schema.define(version: 20150514032623) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "spas", force: :cascade do |t|
-    t.text     "content"
-    t.boolean  "completed"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
+    t.string   "string",                 default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
