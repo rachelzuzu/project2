5680ba650fc29f469f016c36ccb97ba82ba0c061
Rachel Zuzu <rachelzuzu4@gmail.com>
2015-05-14 19:57:25 -0700

    check that graph

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -8,7 +8,7 @@ class Entry < ActiveRecord::Base
 	validates :food, presence: true,
 	length: {minimum: 5}
 
-	validates :mood, presence: true, :numericality => { :greater_than_or_equal_to => -5, :less_than_or_equal_to => 5}
+	validates :mood, presence: true, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 10}
 
 	# paperclip
     has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
